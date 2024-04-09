b2b1efcf449db978836a267f0b571c0f829c1b84
favour orji <favour@titanname.com>
2024-04-09 03:36:42 -0700

    node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - dynamically creates a new node
+ * @parent: pointer to root node
+ * @value: value of the node
+ *
+ * Return:pointer to new node or null
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = value;
+	new->left = NULL;
+	new->right = NULL;
+	new->parent = parent;
+	parent = new;
+
+	return (new);
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+implementation of Binary Tree in C language
