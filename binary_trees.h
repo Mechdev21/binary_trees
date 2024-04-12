b3f50e0f4daa323bb1fbb9cd08719f15b3e61c4c
favour orji <favour@titanname.com>
2024-04-11 20:43:07 -0700

    binary

--- binary_trees.h
+++ binary_trees.h
@@ -23,6 +23,20 @@ struct binary_tree_s
 
 typedef struct binary_tree_s binary_tree_t;
 
+/**
+ * struct queue - simple queue
+ *
+ * @node: is the value
+ * @next: is the next node in the queue
+ */
+struct queue
+{
+	binary_tree_t *node;
+	struct queue *next;
+};
+
+typedef  struct queue queue;
+
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
@@ -40,6 +54,11 @@ size_t binary_tree_leaves(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
+int binary_tree_is_perfect(const binary_tree_t *tree);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 
 
 #endif
